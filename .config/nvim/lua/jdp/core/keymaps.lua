7ae9da5812a22c3fed5654332752c7f52726ce06
Juan David Pineda <jpineda2987@gmail.com>
2025-01-14 16:32:23 -0500

    nvim: add comment for alternative way to do splits

--- .config/nvim/lua/jdp/core/keymaps.lua
+++ .config/nvim/lua/jdp/core/keymaps.lua
@@ -10,15 +10,28 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+-- splits
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split current buffer vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split current buffer horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+-- Below is an alternative way of doing splits (this opens telescope in the new split)
+
+-- keymap.set("n", "<leader>sv", function()
+-- 	vim.cmd("vsplit")
+-- 	vim.cmd("wincmd l")
+-- 	require("telescope.builtin").find_files()
+-- end, { desc = "Vertical split with a new file" })
+-- keymap.set("n", "<leader>sh", function()
+-- 	vim.cmd("split")
+-- 	vim.cmd("wincmd j")
+-- 	require("telescope.builtin").find_files()
+-- end, { desc = "Horizontal split with a new file" })
+
+-- tabs
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
